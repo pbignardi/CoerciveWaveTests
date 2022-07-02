1fdbdfe96ada1f75eafc8e4ec4d2a7a13bf8b8a3
Paolo Bignardi <paolo.bigno@gmail.com>
2022-07-02 09:51:19 +0200

    Add problem parameters to struct. \n General refactor

--- WaveProblem.m
+++ WaveProblem.m
@@ -1,12 +1,17 @@
 function problem = WaveProblem()
     problem = struct();
+    %% Data functions
+    problem.f   = @(x,t) x.*0 + 1;
+    problem.g   = @(x,t) x.*0;
+    problem.u0  = @(x) x.*0;
+    problem.u1  = @(x) x.*0;
     
-    problem.f = @(x,t) x.*0 + 1;
-    problem.g = @(x,t) x.*0;
-    problem.u0 = @(x) x.*0;
-    problem.u1 = @(x) x.*0;
+    %% Parameters
+    problem.c       = 1;
+    problem.theta   = 1;
     
-    problem.u_ex = @(x,t) t.^2 .* (cos(pi * x) + 1);
-    problem.dx_u_ex = @(x,t) t.^2 .* (-pi*sin(pi*x));
-    problem.dt_u_ex = @(x,t) 2 * t .* (cos(pi * x) + 1);
+    %% Exact solution
+    problem.u       = @(x,t) t.^2 .* (cos(pi * x) + 1);
+    problem.dx_u    = @(x,t) t.^2 .* (-pi*sin(pi*x));
+    problem.dt_u    = @(x,t) 2 * t .* (cos(pi * x) + 1);
 end
