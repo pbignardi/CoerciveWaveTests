9ebe81c031bf96023eb1b9a4dda86aa346985c46
Paolo Bignardi <paolo.bigno@gmail.com>
2022-07-07 18:39:19 +0200

    Fixed wrong term in Zv evaluation

--- compute_rhs.m
+++ compute_rhs.m
@@ -10,15 +10,16 @@ function F = compute_rhs(f, mesh, disc, parameters)
     beta    = parameters.beta;
     xi      = parameters.xi;
     Tstar   = parameters.Tstar;
+    T       = parameters.T;
     c       = parameters.c;
 
     % Discretisation parameters
-    xx = disc.xx.';
-    tt = disc.tt.';
     hx = disc.hx;
     ht = disc.ht;
     nx = disc.nx;
     nt = disc.nt;
+    xx = kron(ones(1, (nt + 1)), disc.x);
+    tt = kron(disc.t, ones((nx + 1), 1));
     ndofs = 4 * (nx + 1) * (nt + 1);
     
     % Quadrature variables
@@ -37,7 +38,15 @@ function F = compute_rhs(f, mesh, disc, parameters)
     % Component basis evaluations
     % When using dpsi and ddpsi, remember to divide by hx and ht to account
     % for derivation chain rule scaling.
-    [psi, dpsi, ddpsi] = BasisEvaluation(nq);
+    [psi, dpsi, ddpsi] = BasisEvaluation(xq);
+    
+    psi_T   = psi;
+    dpsi_T  = ht^(-1) * dpsi; 
+    ddpsi_T = ht^(-2) * ddpsi;
+
+    psi_X   = psi;
+    dpsi_X  = hx^(-1) * dpsi;
+    ddpsi_X = hx^(-2) * ddpsi;
     
     %% Element-wise assembly
     F = zeros(ndofs, 1);
@@ -51,12 +60,13 @@ function F = compute_rhs(f, mesh, disc, parameters)
         % Function local evaluation
         f_eval = f(el_xq, el_tq);
         % Evaluate Zv
-        Zv = - el_xq .* kron(psi, dpsi) * hx^(-1) * xi + ...
-            el_tq .* kron(dpsi, psi) * ht^(-1) * beta + ...
-            kron(dpsi, psi) * ht^(-1) * beta * Tstar;
+        Zv = - el_xq .* kron(psi_T, dpsi_X) * xi + ...
+            el_tq .* kron(dpsi_T, psi_X) * beta - ...
+            kron(dpsi_T, psi_X) * beta * Tstar;
         % Evaluate Wv
-        Wv = kron(ddpsi, psi) * ht^(-2) - kron(psi, ddpsi) * c^2;
+        Wv = kron(ddpsi_T, psi_X) - kron(psi_T, ddpsi_X) * c^2;
         % Integrate using Gauss quadrature
-        F(el_dofs) = F(el_dofs) + sum( f_eval .* (A * Wv - Zv) .* wqxt).';
+        F(el_dofs) = F(el_dofs) + ...
+            sum( f_eval .* (- Zv + Wv * A / (T^2)) .* wqxt).';
     end
 end
