87808d34f0cc575b276ff72b0c503b7adcd239ba
Paolo Bignardi <paolo.bigno@gmail.com>
2022-07-07 18:40:47 +0200

    Main file for numerical tests

--- WavesTests.m
+++ WavesTests.m
@@ -1,24 +1,36 @@
 % Coercive wave equation numerical tests
 % Paolo Bignardi 2022
+clc
 clear
 close all
 addpath(genpath("local_stiffness"));
 
 %% Define problem, discretization and mesh
 % Create simple problem
-p = WaveProblem(2);
+p = WaveProblem(4);
 % Define domain
 Q = Domain(-1, 1, 1);
 % Discretise the domain
-nx = 10; nt = 3;
+nx = 80; nt = 80;
 d = Discretization(nx, nt, Q);
 % Build mesh
 mesh = CartesianMesh(d);
 
+%% Custom form parameters
+form = struct();
+form.A  = 2;
+form.nu = 2;
+form.xi = 1;
+form.beta = 10;
+
 %% Solve problem
-u = SolverWaves(p, Q, mesh, d);
+u = SolverWaves(p, Q, mesh, d, form);
 
 %% Plot solution
 [U, X, T] = SolutionEval(u, mesh, d);
 surf(X, T, U);
 xlabel("X"); ylabel("T");
+
+%% Compute errors
+errors = ComputeErrors(u, p, mesh, d);
+errors.L2E
