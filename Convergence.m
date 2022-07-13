911f5c702a9922c038fed6d8d03a6fe2ba391537
Paolo Bignardi <paolo.bigno@gmail.com>
2022-07-13 17:26:32 +0200

    Added convergence plot for H1, Graph, Operator errors and second time and space derivatives in problem definition.

--- Convergence.m
+++ Convergence.m
@@ -6,13 +6,16 @@ addpath(genpath("local_stiffness"));
 
 %% Define problem, discretization and mesh
 % Create simple problem
-p = WaveProblem(4);
+p = WaveProblem(3);
 % Define domain
-Q = Domain(-1, 1, 10);
+Q = Domain(-1, 1, 1);
 
 %% Iterate for different number of elements
 N = [4, 8, 16, 32, 64, 128];
 L2errors = zeros(length(N), 1);
+H1errors = zeros(length(N), 1);
+GRerrors = zeros(length(N), 1);
+OPerrors = zeros(length(N), 1);
 i = 1;
 for n = N 
     % Discretise the domain
@@ -34,6 +37,9 @@ for n = N
     % Compute errors
     errors = ComputeErrors(u, p, mesh, d);
     L2errors(i) = errors.L2E;
+    H1errors(i) = errors.H1E;
+    GRerrors(i) = errors.GRE;
+    OPerrors(i) = errors.OPE;
     i = i + 1;
 end
 
@@ -43,11 +49,23 @@ Ht = Q.T ./ N;
 H = sqrt(Hx .^ 2 + Ht .^ 2);
 
 loglog(H, L2errors, "-d", "LineWidth", 3);
-
 hold on, grid on
-p_l2 = polyfit(log(H), log(L2errors), 1);
+loglog(H, H1errors, "-s", "LineWidth", 3);
+loglog(H, GRerrors, "-^", "LineWidth", 3);
+%loglog(H, OPerrors, "-x", "LineWidth", 3);
 
-legend(strcat("L2 error - order: ", string(p_l2(1))));
+p_l2 = polyfit(log(H), log(L2errors), 1);
+p_h1 = polyfit(log(H), log(H1errors), 1);
+p_op = polyfit(log(H), log(OPerrors), 1);
+p_gr = polyfit(log(H), log(GRerrors), 1);
+
+legend_entries = {strcat("L2 error - order: ", string(p_l2(1))),...
+                  strcat("H1 error - order: ", string(p_h1(1))),...
+                  strcat("LeastSq error - order: ", string(p_op(1)))};%,...
+                  %strcat("Graph error - order: ", string(p_gr(1)))};
+
+legend(legend_entries, ...
+    "Location", "southeast", "NumColumns", 1);
 
 
 %% PRove
--- WaveProblem.m
+++ WaveProblem.m
@@ -1,98 +1,109 @@
 function problem = WaveProblem(prob_num)
     problem = struct();
     switch prob_num
+        %% Null problem
         case 0
-            %% Data functions
+            % Data functions
             problem.f   = @(x,t) x.* 0;
             problem.g   = @(x,t) x.*0;
             problem.u0  = @(x) x.*0;
             problem.u1  = @(x) x.*0;
             
-            %% Parameters
+            % Parameters
             problem.c       = 1;
             problem.theta   = 1;
             
-            %% Exact solution
+            % Exact solution
             problem.u       = @(x,t) 0;
             problem.dx_u    = @(x,t) t.^2 .* (-pi*sin(pi*x));
             problem.dt_u    = @(x,t) 2 * t .* (cos(pi * x) + 1);
+        %% Dirichlet Poisson problem
         case 1
-            %% Data functions
+            % Data functions
             problem.f   = @(x,t) - 2 * (x.^2 + t.^2 - t - 1);
             problem.g   = @(x,t) x.*0;
             problem.u0  = @(x) x.*0;
             problem.u1  = @(x) x.*0;
             
-            %% Parameters
+            % Parameters
             problem.c       = 1;
             problem.theta   = 1;
             
-            %% Exact solution
+            % Exact solution
             problem.u       = @(x,t) (x.^2 - 1) .* (t.^2 - t);
             problem.dx_u    = @(x,t) t.^2 .* (-pi*sin(pi*x));
             problem.dt_u    = @(x,t) 2 * t .* (cos(pi * x) + 1);
+        %% Plane wave problem
+        case 2
+            % Data functions
+            problem.f   = @(x,t) x.*0;
+            problem.g   = @(x,t) x.*0 + t.*0;
+            problem.u0  = @(x) -5*sin(5*x);
+            problem.u1  = @(x) sin(5*x);
+            
+            % Parameters
+            problem.c       = 5;
+            problem.theta   = 0.01;
+            
+            % Exact solution
+            problem.u       = @(x,t) (sin(pi*x).*sin(pi*t));
+            problem.dx_u    = @(x,t) t.^2 .* (-pi*sin(pi*x));
+            problem.dt_u    = @(x,t) 2 * t .* (cos(pi * x) + 1);
+        %% Homogeneous boundary wave problem 1
         case 3
-            %% Data functions
+            % Data functions
             problem.f   = @(x,t) 2 * (cos(pi * x) + 1) + ...
                                 t.^2 .* (pi^2 * cos(pi*x));
             problem.g   = @(x,t) x.*0;
             problem.u0  = @(x) x.*0;
             problem.u1  = @(x) x.*0;
             
-            %% Parameters
+            % Parameters
             problem.c       = 1;
             problem.theta   = 1;
             
-            %% Exact solution
+            % Exact solution
             problem.u       = @(x,t) t.^2 .* (cos(pi * x) + 1);
             problem.dx_u    = @(x,t) t.^2 .* (-pi*sin(pi*x));
             problem.dt_u    = @(x,t) 2 * t .* (cos(pi * x) + 1);
+            problem.ddt_u   = @(x,t) 2*cos(pi*x) + 2;
+            problem.ddx_u   = @(x,t) -t.^2 .* cos(pi*x) * pi^2;
+        %% Homogeneous boundary wave problem 2
         case 4
-            %% Data functions
+            % Data functions
             problem.f   = @(x,t) 2 * (cos(pi * x) + 1) .* cos(2*t) + ...
                         pi^2 * cos(pi * x) .* sin(t).^2;
             problem.g   = @(x,t) x.*0;
             problem.u0  = @(x) x.*0;
             problem.u1  = @(x) x.*0;
             
-            %% Parameters
+            % Parameters
             problem.c       = 1;
             problem.theta   = 1;
             
-            %% Exact solution
-            problem.u    = @(x,t) sin(t).^2 .* (cos(pi*x) + 1);
-            problem.dx_u = @(x,t) t.^2 .* (-pi*sin(pi*x));
-            problem.dt_u = @(x,t) 2 * t .* (cos(pi * x) + 1);
-        case 5
-            %% Data functions
-            problem.f   = @(x,t) x.*0 + 1;
-            problem.g   = @(x,t) x.*0;
-            problem.u0  = @(x) x.*0;
-            problem.u1  = @(x) x.*0;
-            
-            %% Parameters
-            problem.c       = 1;
-            problem.theta   = 1;
-            
-            %% Exact solution
-            problem.u    = @(x,t) sin(t).^2 .* (cos(pi*x) + 1);
-            problem.dx_u = @(x,t) t.^2 .* (-pi*sin(pi*x));
-            problem.dt_u = @(x,t) 2 * t .* (cos(pi * x) + 1);
-        case 2
-            %% Data functions
-            problem.f   = @(x,t) 2*pi^2 * (sin(pi*x).*sin(pi*t));
-            problem.g   = @(x,t) x.*0;
-            problem.u0  = @(x) x.*0;
-            problem.u1  = @(x) x.*0;
-            
-            %% Parameters
-            problem.c       = 1;
-            problem.theta   = 1;
-            
-            %% Exact solution
-            problem.u       = @(x,t) (sin(pi*x).*sin(pi*t));
+            % Exact solution
+            problem.u       = @(x,t) sin(t).^2 .* (cos(pi*x) + 1);
             problem.dx_u    = @(x,t) t.^2 .* (-pi*sin(pi*x));
             problem.dt_u    = @(x,t) 2 * t .* (cos(pi * x) + 1);
+            problem.ddt_u   = @(x,t) 2*cos(t).^2.*(cos(pi*x) + 1) - ...
+                2*sin(t).^2 .* (cos(pi*x) + 1);
+            problem.ddx_u   = @(x,t) -pi^2 * cos(pi*x) .* sin(t).^2;
+        %% Non-homogeneous boundary
+        case 5
+            % Data functions
+            problem.f   = @(x,t) x.*0;
+            problem.g   = @(x,t) x.*0;
+            problem.u0  = @(x) -sin(x);
+            problem.u1  = @(x) x.*0;
+            
+            % Parameters
+            problem.c       = 1;
+            problem.theta   = 1;
+            
+            % Exact solution
+            problem.u    = @(x,t) sin(t).^2 .* (cos(pi*x) + 1);
+            problem.dx_u = @(x,t) t.^2 .* (-pi*sin(pi*x));
+            problem.dt_u = @(x,t) 2 * t .* (cos(pi * x) + 1);
         otherwise
             %warning("No matching problem number");
     end
