2967726b0399d9033069cbdda6e2522f1ab09e94
Paolo Bignardi <paolo.bigno@gmail.com>
2022-07-23 14:56:49 +0200

    Modified to output a GIF at 24 fps

--- animate.m
+++ animate.m
@@ -4,9 +4,20 @@ function [] = animate(X, T, U)
     %   time variable t.
     
     %% Parameters
+    % The number of time steps is given by the number of rows of U
+    n_tsteps = size(U,1);
+
     % Frame rate
-    delay = 1/30;
+    duration_mult = 2;
+    fps = 24;
+    T_max = max(max(T));
+
+    actual_duration = n_tsteps / fps;
+    target_nframes = duration_mult * T_max * fps;
+    skip_nframes = floor(size(T, 1) / target_nframes);
     
+    frames = 1:skip_nframes:size(T,1);
+
     % U_lims
     u_min = min(min(U));
     u_max = max(max(U));
@@ -15,19 +26,19 @@ function [] = animate(X, T, U)
     x_min = min(min(X));
     x_max = max(max(X));
 
-    % The number of time steps is given by the number of rows of U
-    n_tsteps = size(U,1);
+    
 
     %% Plotting
     % Plot each row of U
-    for i = 1:n_tsteps
-        x = X(i, :);
-        u = U(i, :);
+    for f = frames
+        x = X(f, :);
+        u = U(f, :);
         plot(x, u);
         axis([x_min x_max u_min u_max]);
-        grid on
         drawnow
-        pause(delay);
+        pause(1/fps);
+        %exportgraphics(gcf,"solution.gif","Append", true);
+        grid on
     end
 
 end
