8cb7597b9274320f1fcfa0120038c9e592540621
Paolo Bignardi <paolo.bigno@gmail.com>
2022-09-03 16:02:15 +0200

    Updated domain structure to allow for 2d domains, and computed delta for general 2d domain and degenerate 2d domains (segments)

--- Domain.m
+++ Domain.m
@@ -1,8 +1,40 @@
-function domain = Domain(xmin, xmax, T)
+function domain = Domain(T, xmin, xmax, varargin)
     domain = struct();
+    
+    if length(varargin) == 2
+        domain.ymin = varargin{1};
+        domain.ymax = varargin{2};
+    elseif isempty(varargin)
+        domain.ymin = 0;
+        domain.ymax = 0;
+    else
+        warning("Check domain of y variable!");
+    end
     domain.xmin = xmin;
     domain.xmax = xmax;
     domain.T = T;
-    domain.L = abs(xmin-xmax);
-    domain.delta = min(-1*xmin, xmax) / domain.L;
+    domain.L = norm([domain.xmax domain.ymax] - [domain.xmin domain.ymin]);
+    vertices = [domain.xmin    domain.ymin;
+                domain.xmax    domain.ymin;
+                domain.xmax    domain.ymax;
+                domain.xmin    domain.ymax].';
+    delta = 1;
+    % 
+    for v = vertices
+        % Ignore component with a null component
+        if v(1) ~= 0
+            v1 = v(1);
+        else
+            v1 = v(2);
+        end
+        if v(2) ~= 0
+            v2 = v(2);
+        else
+            v2 = v(1);
+        end
+        % (x . n) is picking one of the two component, ignore the ones with
+        % a null component
+        delta = min(delta, min(abs(v1), abs(v2)));
+    end
+    domain.delta = delta / domain.L;
 end
