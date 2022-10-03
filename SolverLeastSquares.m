367b2bb97ae4b797e40f7bf129c143fa9a36f300
Paolo Bignardi <paolo.bigno@gmail.com>
2022-10-03 16:02:49 +0200

    Added files for least square problems

---
+++ SolverLeastSquares.m
@@ -0,0 +1,104 @@
+function [u, Kcond] = SolverLeastSquares(problem, domain, mesh, disc)
+    %% Unpacking parameters
+    % Problem unpacking
+    c       = problem.c;
+    theta   = problem.theta;
+
+    % Elements unpacking
+    hx = disc.hx;
+    ht = disc.ht;
+   
+
+    % Boundary elms unpacking
+    right_elms  = mesh.right_elms;
+    left_elms   = mesh.left_elms;
+    bot_elms    = mesh.bot_elms;
+    
+    %% Local operator structs
+    Xb = HermiteBasis(hx);
+    Tb = HermiteBasis(ht);
+
+    %% Local evaluation structs
+    Ea = HermiteEvaluation(hx, 0);
+    Eb = HermiteEvaluation(hx, 1);
+    E0 = HermiteEvaluation(ht, 0);
+
+    %% Local operator over Q
+    % Lap u * Lap v
+    opQ = kron(Tb.d0d0, Xb.d2d2) * c^4;
+    % utt * vtt
+    opQ = opQ + kron(Tb.d2d2, Xb.d0d0);
+    % - c^2 Lap u * vtt
+    opQ = opQ - c^2 * kron(Tb.d0d2, Xb.d2d0);
+    % - c^2 Lap v * utt
+    opQ = opQ - c^2 * kron(Tb.d2d0, Xb.d0d2);
+
+
+    %% Local operator over Omega_0
+    % u(0) v(0)
+    opO = kron(E0.d0d0, Xb.d0d0);
+    % ut(0) vt(0)
+    opO = opO + kron(E0.d1d1, Xb.d0d0);
+
+    %% Local operator over Sigma_a
+    n = -1;
+    % dn u * dn v
+    opSa = kron(Tb.d0d0, Ea.d1d1);
+    % 1/(theta * c)^2 * ut * vt
+    opSa = opSa + kron(Tb.d1d1, Ea.d0d0) * (theta * c)^(-2);
+    % 1/(theta * c) dn u * vt
+    opSa = opSa + kron(Tb.d0d1, Ea.d1d0) * (theta * c)^(-1) * n;
+    % 1/(theta * c) dn v * ut
+    opSa = opSa + kron(Tb.d1d0, Ea.d0d1) * (theta * c)^(-1) * n;
+    
+    %% Local operator over Sigma_b
+    n = 1;
+    % dn u * dn v
+    opSb = kron(Tb.d0d0, Eb.d1d1);
+    % 1/(theta * c)^2 * ut * vt
+    opSb = opSb + kron(Tb.d1d1, Eb.d0d0) * (theta * c)^(-2);
+    % 1/(theta * c) dn u * vt
+    opSb = opSb + kron(Tb.d0d1, Eb.d1d0) * (theta * c)^(-1) * n;
+    % 1/(theta * c) dn v * ut
+    opSb = opSb + kron(Tb.d1d0, Eb.d0d1) * (theta * c)^(-1) * n;
+    
+
+    %% Internal matrix assembly
+    KQ = assembleLS(opQ, mesh, disc);
+
+    %% Boundary matrix assembly
+    % Omega0 boundary
+    K0 = assembleLS_boundary(opO, mesh, disc, bot_elms);
+    % Sigma = a boundary
+    Ka = assembleLS_boundary(opSa, mesh, disc, left_elms);
+    % Sigma = b boundaryu
+    Kb = assembleLS_boundary(opSb, mesh, disc, right_elms);
+
+
+    %% Global matrix computation
+    K = KQ + K0 + Ka + Kb;
+
+    %% Load vector assembly
+    F = computeLS_rhs(problem, mesh, disc, domain);
+
+    %% Solving
+    % Start timer
+    tic
+    %u = gmres(K, F);
+    u = K \ F;
+    %u = lsqr(K, F);
+    
+    solving_time = toc;
+    % Log time result
+    fprintf("Linear system solving time:\t %.4f seconds\n", solving_time);
+    
+    %% Post-processing solution
+    % Compute condest of matrix K :TODO
+    %Kcond = condest(K(internal,internal));
+    Kcond = condest(K);
+
+    % Compute mean
+    omega0_mean = ComputeMean(u, 0, mesh, disc);
+    % Remove computed mean
+    %u = u - omega0_mean;
+end
---
+++ computeLS_rhs.m
@@ -0,0 +1,165 @@
+function F = computeLS_rhs(p, mesh, disc, domain)
+    %% Unpacking parameters
+    % Mesh parameters
+    elms    = mesh.elms;
+    pivots  = mesh.pivots;
+    n_elms  = size(elms, 1);
+
+    % Boundary elms
+    bot_elms    = mesh.bot_elms;
+    left_elms   = mesh.left_elms;
+    right_elms  = mesh.right_elms;
+
+    % Problem parameters
+    f       = p.f;
+    g       = p.g;
+    u0      = p.u0;
+    u1      = p.u1;
+    c       = p.c;
+    theta   = p.theta; 
+
+    % Formulation parameters
+    a = domain.xmin;
+    b = domain.xmax;
+
+    % Discretisation parameters
+    hx = disc.hx;
+    ht = disc.ht;
+    nx = disc.nx;
+    nt = disc.nt;
+    xx = kron(ones(1, (nt + 1)), disc.x);
+    tt = kron(disc.t, ones((nx + 1), 1));
+    ndofs = 4 * (nx + 1) * (nt + 1);
+    
+    % Quadrature variables
+    nq = 8;
+    [xq, wq] = gaussquad(nq);
+
+    % Local quadrature nodes and weights
+    xqh = hx * xq;
+    tqh = ht * xq;
+    wqx = hx * wq;
+    wqt = ht * wq;
+    wqxt = kron(wqt, wqx);
+    xxqh = kron(ones(nq, 1), xqh);
+    ttqh = kron(tqh, ones(nq, 1));
+    %% Component basis functions evaluation
+    % Component basis evaluations
+    % When using dpsi and ddpsi, remember to divide by hx and ht to account
+    % for derivation chain rule scaling.
+    [psi, dpsi, ddpsi] = BasisEvaluation(xq);
+    
+    % Basis function evaluation over grid
+    psi_T   = psi;
+    dpsi_T  = ht^(-1) * dpsi; 
+    ddpsi_T = ht^(-2) * ddpsi;
+
+    psi_X   = psi;
+    ddpsi_X = hx^(-2) * ddpsi;
+    
+    % Basis function evaluation at boundary
+    Estart_d0 = [1, 0, 0, 0];
+    Estart_d1 = [0, 0, 1, 0];
+    Eend_d0 = [0, 1, 0, 0];
+    Eend_d1 = [0, 0, 0, 1];
+    
+    Ea_d0 = Estart_d0;
+    Ea_d1 = Estart_d1 * hx^(-1);
+    Eb_d0 = Eend_d0;
+    Eb_d1 = Eend_d1 * hx^(-1);
+    E0_d0 = Estart_d0;
+    E0_d1 = Estart_d1 * ht^(-1);
+    
+    %% Operator evaluations
+    % Q local terms
+    vtt_Q   = kron(ddpsi_T, psi_X);
+    Lapv_Q  = kron(psi_T, ddpsi_X);
+    
+    % Omega0 local terms
+    vt0     = kron(E0_d1, psi_X);
+    v0      = kron(E0_d0, psi_X);
+
+    % Sigma=a local terms
+    vta     = kron(dpsi_T, Ea_d0);
+    vxa     = kron(psi_T, Ea_d1);
+   
+    % Sigma=b local terms
+    vtb     = kron(dpsi_T, Eb_d0);
+    vxb     = kron(psi_T, Eb_d1);
+
+    %% Q- Element-wise assembly
+    F = zeros(ndofs, 1);
+    for e = 1:n_elms
+        % Get element nodes and DOFs
+        el_ids  = elms(e, :);
+        el_dofs = mapper(el_ids, nx, nt);
+        % Move quadrature nodes
+        el_xq = xxqh + xx(pivots(e));
+        el_tq = ttqh + tt(pivots(e));
+        % Function f local evaluation
+        f_eval = f(el_xq, el_tq);
+        
+        %% Q domain integral
+        % Compute Wv
+        Wv = vtt_Q - Lapv_Q * c^2;
+        % Integrate using Gauss quadrature
+        local_rhs_Q = sum( f_eval .* Wv .* wqxt ).';
+        
+        F(el_dofs) = F(el_dofs) + local_rhs_Q;
+    end
+
+    %% Omega0- Element-wise assembly
+    for e = bot_elms
+        % Get element nodes and DOFs
+        el_ids  = elms(e, :);
+        el_dofs = mapper(el_ids, nx, nt);
+        
+        % Move quadrature nodes
+        el_xq = xxqh(1:nq) + xx(pivots(e));
+    
+        % u1 and u0x evaluation 
+        U0 = u0(el_xq);
+        U1 = u1(el_xq);
+
+        % Integrand evaluation
+        integrand = U1 .* vt0 + U0 .* v0;
+        local_rhs_0 = sum( integrand .* wqx ).';  
+        F(el_dofs) = F(el_dofs) + local_rhs_0;
+    end
+
+    %% Sigma=a- Element-wise assembly
+    for e = left_elms
+        % Get element nodes and DOFs
+        el_ids  = elms(e, :);
+        el_dofs = mapper(el_ids, nx, nt);
+
+        % Move quadrature nodes
+        el_tq = ttqh(nq * (1:nq)) + tt(pivots(e));
+
+        % g evaluation
+        G = g(a, el_tq);
+
+        % Impedance boundary evaluation
+        test_impedance = - vxa + vta / (theta * c);
+        local_rhs_a = sum(test_impedance .* G .* wqt).';
+        F(el_dofs) = F(el_dofs) - c^2 * local_rhs_a;
+    end
+
+    %% Sigma=b- Element-wise assembly
+    for e = right_elms
+        % Get element nodes and DOFs
+        el_ids  = elms(e, :);
+        el_dofs = mapper(el_ids, nx, nt);
+
+        % Move quadrature nodes
+        el_tq = ttqh(nq*(1:nq)) + tt(pivots(e));
+
+        % g evaluation
+        G = g(b, el_tq);
+
+        % Zv boundary evaluation
+        test_impedance = vxb + vtb / (theta * c);
+        local_rhs_b = sum(test_impedance .* G .* wqt).';
+        F(el_dofs) = F(el_dofs) + local_rhs_b;
+    end
+end
