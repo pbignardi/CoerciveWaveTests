2f25aac97161abd15159c6d71fbdf0c1ea3a790f
Paolo Bignardi <paolo.bigno@gmail.com>
2022-10-18 00:53:59 +0200

    Fixed error in output file name

--- ParameterATesting.m
+++ ParameterATesting.m
@@ -10,10 +10,10 @@ p = WaveProblem(p_num);
 % Define domain
 Q = Domain(-1, 1, 1);
 
-solver = "LS";
+solver = "CO";
 
 %% Grid of the parameter $A$
-N = 300;
+N = 10;
 As = logspace(-15, 2, N);
 %As = logspace(log10(0.0000000001), log10(1), 2);
 L2errors = zeros(length(As), 1);
@@ -23,23 +23,17 @@ Kconds = zeros(length(As), 1);
 i = 1;
 for A = As
     % Define number of elements
-    nx = 256; nt = 256;
+    nx = 128; nt = 128;
     % Define discretisation
     d = Discretization(nx, nt, Q);
     % Build mesh
     mesh = CartesianMesh(d);
     
-    form = struct();
-    form.A = A;
-    form.nu = 2;
-    form.xi = 1;
-    form.beta = 20;
-
     % Solve problem
     if solver == "LS"
          [u, Kcond] = SolverLeastSquares(p, Q, mesh, d, A);
     elseif solver == "CO"
-         [u, Kcond] = SolverWaves(p, Q, mesh, d, form);
+         [u, Kcond] = SolverWaves(p, Q, mesh, d, A);
     end
     %omega0_mean = ComputeMean(u, 0, mesh, d);
     %u = MeanShift(u, omega0_mean, mesh);
@@ -53,4 +47,5 @@ end
 A = As.';
 results = table(A, L2errors, Kconds);
 writetable(results, ...
-    strcat("test_results/p",string(p_num), "_",string(solver),"_par_A_N", string(N),.dat"));
+    strcat("test_results/p",string(p_num), "_",string(solver), ...
+            "_par_A_N", string(N),".dat"));
