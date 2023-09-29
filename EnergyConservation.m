fab84549f4e5fa00210d5be8369d2a2a97182eef
Paolo Bignardi <paolo.bigno@gmail.com>
2023-09-29 10:45:43 +0200

    cleanup energy conservation tests

--- EnergyConservation.m
+++ EnergyConservation.m
@@ -18,29 +18,59 @@ mesh = CartesianMesh(d);
 
 %% Custom form parameters
 form = initializeForm(p, Q, 'OPT');
+
+%% Print info
+% TODO
 %% Solve problem
 u = SolverWaves(p, Q, mesh, d, form);
 [~, uproj, ~] = ProjectionBFS(p, d);
-%% Plot solution
-[U, X, T] = SolutionGridEval(u, mesh, d, linspace(p.Q.xmin, p.Q.xmax, 201), ...
-    linspace(0, p.Q.T, 201));
-[Uproj, ~, ~] = SolutionGridEval(uproj, mesh, d, linspace(p.Q.xmin, p.Q.xmax, 201),...
-    linspace(0, p.Q.T, 201));
-% pcolor(X, T, (p.u(X, T) - U));
-pcolor(X, T, U);
-colorbar
-% surf(X, T, U);
-shading flat; 
-xlabel("X"); ylabel("T");
 
 %% Compute energy at each time step
-[u_energy, T] = ComputeEnergy(u, mesh, d, p);
+Ts = linspace(0, 1, 400);
+Eu = ComputeEnergy(u, Ts, mesh, d, p);
 
-figure
-u_ex_energy = zeros(size(u_energy));
-for i = 1:numel(T)
-    u_ex_energy(i) = ComputeExactEnergy(T(i), p);
+figure(1)
+Eu_ex = zeros(size(Eu));
+for i = 1:numel(Ts)
+    Eu_ex(i) = ComputeExactEnergy(Ts(i), p);
 end
-plot(T, u_energy - u_ex_energy);
+plot(Ts, Eu);
+hold on
+plot(Ts, Eu_ex);
+hold off
+title('Energy computed using time-slices');
 
-%% Compute energy error
+figure(2)
+error = abs(Eu - Eu_ex);
+err_type = 'Absolute';
+if all(Eu_ex ~= 0)
+    error = error ./ Eu_ex;
+    err_type = 'Relative';
+end
+plot(Ts, error);
+title([err_type, ' error of energy using time-slices']);
+
+%% Compute energy using time quadrature points as evaluation
+figure(3)
+[Eu_qp, Ts_qp] = ComputeEnergyOnGrid(u, mesh, d, p);
+Eu_ex_qp = zeros(size(Eu_qp));
+for i = 1:numel(Eu_ex_qp)
+    Eu_ex_qp(i) = ComputeExactEnergy(Ts_qp(i), p);
+end
+plot(Ts_qp, Eu_qp);
+hold on
+plot(Ts_qp, Eu_ex_qp);
+hold off
+title('Energy evaluated at time quadrature points');
+
+figure(4)
+error = abs(Eu_qp - Eu_ex_qp);
+err_type = 'Absolute';
+if all(Eu_ex_qp ~= 0)
+    error = error ./ Eu_ex_qp;
+    err_type = 'Relative';
+end
+plot(Ts_qp, error);
+title([err_type, ' error of energy using time quadrature points']);
+
+
