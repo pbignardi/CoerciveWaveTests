a504942817970bcce5851da53e5c37fb9f82a1a6
Paolo Bignardi <paolo.bigno@gmail.com>
2023-11-25 20:52:01 +0100

    Unified all energy computations in single function

--- src/ComputeEnergy.m
+++ src/ComputeEnergy.m
@@ -1,5 +1,19 @@
-function E = ComputeEnergy(u, t, mesh, disc, problem)
-    %% Parameters unpacking
+function varargout = ComputeEnergy(u, t, mesh, disc, problem)
+% Compute the energy of the discrete solution, of the exact solution and of the
+% numerical error E(t; u-u_h)
+%
+% INPUTS:
+%   u: (float[]) dofs of the discrete solution
+%   t: (float[]) vector of time evaluations where to compute energy
+%   mesh: (struct) mesh of the domain (see CartesianMesh.m)
+%   disc: (struct) discretization of the domain (see Discretization.m)
+%   problem: (struct) wave problem struct (see WaveProblem.m)
+%
+% OUTPUT:
+%   E_h: (float[])_energy of the discrete solution
+%   E: (float[])_energy of the exact solution
+%   error_energy: (float[]) energy of the error at each timestep t
+
     % Discretisation parameters
     nx = disc.nx;
     nt = disc.nt;
@@ -7,7 +21,10 @@ function E = ComputeEnergy(u, t, mesh, disc, problem)
 
     % Problem parameters
     c = problem.c;
+    dx_u = @problem.dx_u;
+    dt_u = @problem.dt_u;
 
+    % Quadrature data
     nq = 9;
     [xq, wq] = gaussquad(nq);
     wqx = reshape(wq, 1, []) * hx;
@@ -19,16 +36,26 @@ function E = ComputeEnergy(u, t, mesh, disc, problem)
     Omegat_elms = reshape(mesh.bot_elms, 1, []);
     for k = 1:numel(t)
         while i < nt && ~(disc.t(i) <= t(k) && disc.t(i + 1) > t(k))
-            i = i + 1;
-            Omegat_elms = Omegat_elms + nx;
+        i = i + 1;
+        Omegat_elms = Omegat_elms + nx;
         end
-       
-        
-        % evaluate operators
+
+        % map point t(k) into reference element
         tloc = (t(k) - disc.t(i))/disc.ht;
-        eval_points = {xq, tloc};
-        Ux = OperatorEval(u, mesh, disc, eval_points, 'ux', Omegat_elms);
-        Ut = OperatorEval(u, mesh, disc, eval_points, 'ut', Omegat_elms);
-        E(k) = 1/2 * (c^2 * Ux .* Ux + Ut .* Ut) * wqx.';
+        % construct xt-evaluation points
+        eval_nodes = {xq, tloc};
+
+        [Ux, X, T] = OperatorEval(u, mesh, disc, eval_nodes, 'ux', Omegat_elms);
+        [Ut, ~, ~] = OperatorEval(u, mesh, disc, eval_nodes, 'ut', Omegat_elms);
+        Ux_ex = dx_u(X, T);
+        Ut_ex = dt_u(X, T);
+
+        discrete_energy(k) = 1/2 * (c^2 * (Ux).^2 + (Ut).^2) * wqx.';
+        exact_energy(k) = 1/2 * (c^2 * (Ux_ex).^2 + (Ut_ex).^2) * wqx.';
+        error_energy(k) = 1/2 * (c^2*(Ux - Ux_ex).^2 + (Ut - Ut_ex).^2) * wqx.';
     end
+
+    varargout{1} = discrete_energy;
+    varargout{2} = exact_energy;
+    varargout{3} = error_energy;
 end
