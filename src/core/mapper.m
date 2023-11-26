b9f42e69a772a2c9d7304fe5cb7bbfe98eb65ec2
Paolo Bignardi <paolo.bigno@gmail.com>
2023-11-26 11:11:11 +0100

    refactored dofmapper
    Using repmat instead of hard-coded mapping of dofs.
    split reorder vector of dofs to improve readability.

--- src/core/mapper.m
+++ src/core/mapper.m
@@ -1,21 +1,19 @@
 function dofs = mapper(el_ids, nx, nt)
-    % map nodes_ids to degrees of freedom
+    % map nodes_to degrees of freedom
     % The order is:
     % v(1) v(2) dxv(1) dxv(2)
     % v(3) v(4) dxv(3) dxv(4)
     % dyv(1) dyv(2) dxyv(1) dxvy(2)
     % dyv(3) dyv(4) dxyv(3) dxyv(4)
     n_nodes = (nx + 1) * (nt + 1);
-    % dofs = kron(el_ids, [1,1,1,1]) + n_nodes * kron([1,1,1,1], 0:3);
-    dofs = [el_ids(1) el_ids(1) el_ids(1) el_ids(1) ...
-            el_ids(2) el_ids(2) el_ids(2) el_ids(2) ...
-            el_ids(3) el_ids(3) el_ids(3) el_ids(3) ...
-            el_ids(4) el_ids(4) el_ids(4) el_ids(4)] + ...
-           [0   n_nodes     2 * n_nodes   3 * n_nodes ...
-            0   n_nodes     2 * n_nodes   3 * n_nodes ...
-            0   n_nodes     2 * n_nodes   3 * n_nodes ...
-            0   n_nodes     2 * n_nodes   3 * n_nodes];
-    % Shuffle the elements to get dofs [v(i), dxv(i), dyv(i), dxyv(i)]
-    shuffle = [1, 5, 2, 6, 9, 13, 10, 14, 3, 7, 4, 8, 11, 15, 12, 16];
+    el_base = repmat(el_ids, 4, 1);
+    dofs_shift = repmat([0;n_nodes;2*n_nodes;3*n_nodes], 1, 4);
+    dofs = reshape(el_base + dofs_shift, 1, []);
+    % Shuffle the elements to get dofs ordered [v(i), dxv(i), dyv(i), dxyv(i)]
+    v_dofs = [1, 5, 2, 6];
+    dxv_dofs = [9, 13, 10, 14];
+    dyv_dofs = [3, 7, 4, 8];
+    dxyv_dofs = [11, 15, 12, 16];
+    shuffle = [v_dofs, dxv_dofs, dyv_dofs, dxyv_dofs];
     dofs = dofs(shuffle);
 end
